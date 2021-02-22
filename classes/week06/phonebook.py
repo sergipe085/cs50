5e71b88c03266c72fda58b3ae8c99199d962bfd4
Sérgio <dapscreed@gmail.com>
2021-02-22 23:52:33 +0000

    phonebook.py v2

--- classes/week06/phonebook.py
+++ classes/week06/phonebook.py
@@ -1,18 +1,13 @@
+import csv
+
 from cs50 import get_string
-import sys
 
-phonebook = {
-    "Jorge": "+55 85 94587-1234",
-    "Pao": "+55 85 99999-9999"
-}
-
-s = get_string("Name: ")
-
-if s in phonebook:
-    number = phonebook[s]
-    print(f"{number}")
-    sys.exit(0)
-print("Name not founded")    
-sys.exit(1)
+with open("./phonebook.csv", "a") as file:
     
+    name = get_string("Name: ")
+    number = get_string("Number: ")
+    
+    writer = csv.writer(file)
+    writer.writerow([name, number])
+
     
